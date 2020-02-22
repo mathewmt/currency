517a360c2b8acdf644932975844d1165d4b90a17
mathewmt <mathewmt015@gmail.com>
2020-02-22 21:07:57 +0530

    changes in database config and package.json

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb+srv://mathew:mathew123%231@cluster0-ps3u1.mongodb.net/test?retryWrites=true&w=majority',
+    url: 'mongodb+srv://mathew:mathew123%231@cluster0-ps3u1.mongodb.net/currency?retryWrites=true&w=majority',
     baseurl:'http://localhost:5000'
 }
 
