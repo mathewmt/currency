aabfca0f0539ef920965a6c9c40b7310192ffd90
mathewmt <mathewmt015@gmail.com>
2020-02-22 21:16:06 +0530

    heroku based port and url changes

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     url: 'mongodb+srv://mathew:mathew123%231@cluster0-ps3u1.mongodb.net/currency?retryWrites=true&w=majority',
-    baseurl:'http://localhost:5000'
+    baseurl:'https://counterfeit.herokuapp.com'
 }
 
